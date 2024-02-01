b0867372746f5adfdb12150672be3f817aa3bc96
sreelekhab77 <sreelekha789sl@gmail.com>
2024-02-01 12:40:04 +0530

    first commit

---
+++ src/test/java/google/cicddemo/googleautomation.java
@@ -0,0 +1,19 @@
+package google.cicddemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class googleautomation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver =new FirefoxDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		
+	}
+
+}
